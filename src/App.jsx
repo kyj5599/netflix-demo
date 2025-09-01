1d63e4375955d43fed74d425be09fd097cce7936
yujin <kimyujin.ked@baenergy.net>
2025-09-01 17:32:23 +0900

    ✨ Route 구성하기: nested route와 Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFound from "./pages/NotFoundPage/NotFound";
 
+// 홈페이지  /
+// 영화 전체보여주는 페이지(서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// 추천영화 /movies/:id/recommendations
+// 리뷰 /movies/:id/reviews
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
