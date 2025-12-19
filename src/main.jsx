cf98ea0570539f92239922b31adb162c647db5aa
kyj5599 <277k@naver.com>
2025-12-19 18:52:40 +0900

    ✨Banner 만들기(hook-usePopularMovies.js)

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Banner from "./components/Banner/Banner";
+
+// 1. 배너 => popular movie 중에서 랜덤으로 하나 선택
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Homepage;
