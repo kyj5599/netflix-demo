e552bfc5382561aa77d1f81ea259e8228ae990e3
kyj5599 <277k@naver.com>
2025-12-21 17:32:21 +0900

    ✨영화 슬라이드 구현

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./components/Banner/Banner";
+import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 
 // 1. 배너 => popular movie 중에서 랜덤으로 하나 선택
 // 2. popular movie
@@ -10,6 +11,7 @@ const Homepage = () => {
   return (
     <div>
       <Banner />
+      <PopularMovieSlide />
     </div>
   );
 };
