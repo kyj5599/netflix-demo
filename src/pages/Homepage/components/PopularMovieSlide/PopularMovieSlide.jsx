5aa93d217b048f9ca7c77874b3eb5e542a2b2fe6
kyj5599 <277k@naver.com>
2025-12-31 17:40:20 +0900

    💄 영화 카드 캐러설 디자인 수정

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -9,17 +9,17 @@ import "./PopularMovieSlide.style.css";
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 6,
+    items: 8,
     partialVisibilityGutter: 0,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
+    items: 4,
     partialVisibilityGutter: 0,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1,
+    items: 2,
     partialVisibilityGutter: 0,
   },
 };
